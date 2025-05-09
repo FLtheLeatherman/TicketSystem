3f28b6057054c056e7fb88a45d9a849718e271c5
FLtheLeatherman <627720734@qq.com>
2025-05-09 23:08:18 +0800

    initial commit

---
+++ build/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/istina/TicketSystem/prework/BPT.cpp" "CMakeFiles/code.dir/prework/BPT.cpp.o" "gcc" "CMakeFiles/code.dir/prework/BPT.cpp.o.d"
+  "/home/istina/TicketSystem/prework/MemoryRiver.cpp" "CMakeFiles/code.dir/prework/MemoryRiver.cpp.o" "gcc" "CMakeFiles/code.dir/prework/MemoryRiver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/prework/BPT.cpp.o"
+  "CMakeFiles/code.dir/prework/BPT.cpp.o.d"
+  "CMakeFiles/code.dir/prework/MemoryRiver.cpp.o"
+  "CMakeFiles/code.dir/prework/MemoryRiver.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
